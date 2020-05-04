ad574b10aaa6403560505a1a50f4e7b3addfd09c
mwataru-pro <m1130wata@gmail.com>
2020-05-04 21:54:56 +0900

    Initial Commit

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '79f10e6e14f93b96e3c1b067dd6aa9146ccccbc610d90d081e4ef8d6809c2e35a0b20991d7f41cec47aaf846576ec4a0eb60d59aa3137641d98d63dc3c218a37'
